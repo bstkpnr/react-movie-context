8784225af4132b482bfcd88fddf29ae06d47c889
Beste Tokpınar <55047664+bstkpnr@users.noreply.github.com>
2021-12-27 10:05:53 +0300

    added context

---
+++ src/Context/ContextMovie.js
@@ -0,0 +1,26 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
+
+const MoviesContext = createContext();
+
+const MoviesProvider = ({ children }) => {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  useEffect(() => {
+    axios
+      .get("https://imdb-api.com/en/API/MostPopularMovies/k_9jopu1jg")
+      .then((response) => setMovies(response.data.items))
+      .catch((error) => alert(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  const values = { movies, isLoading };
+
+  return (
+    <MoviesContext.Provider value={values}>{children}</MoviesContext.Provider>
+  );
+};
+
+const useMovies = () => useContext(MoviesContext);
+
+export { MoviesProvider, useMovies };
